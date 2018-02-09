9092104bbd933d4fac437ae9226f3a07a157788a
Rachel Elberger <relberger96@gmail.com>
2018-02-08 20:03:21 -0500

    Added vendingmachine/VendingMachine

--- src/elberger/vendingmachine/VendingMachine.java
+++ src/elberger/vendingmachine/VendingMachine.java
@@ -2,17 +2,59 @@ package elberger.vendingmachine;
 
 public class VendingMachine
 {
+	int quarters;
+	int nickels; 
+	int dimes;
+	int pennies;
+	double totalChange;
+	double remainingChange;
+
+	public double calculateTotalChange(double price, double paid)
+	{
+		totalChange = paid - price;
+		return totalChange;
+	}
+	public double calculateRemainingChange(int coin)
+	{
+		remainingChange = totalChange - coin;
+		return remainingChange;
+	}
+	
+	public int calculateQuarters()
+	{
+		quarters = (int) (totalChange / 25);
+		return quarters;
+	}
+
+	public int calculateDimes()
+	{
+		dimes = (int) (calculateRemainingChange(quarters) / 10);
+		return dimes;
+	}
+
+	public int calculateNickels()
+	{
+		nickels = (int) (calculateRemainingChange(dimes) / 5);
+		return nickels;
+	}
+
+	public int calculatePennies()
+	{
+		pennies = (int) (calculateRemainingChange(nickels) / 1);
+		return pennies;
+	}
+
 
 	Change pay(double price, double paid)
 	{
-		Change tmp = new Change (0,0,0,0);	
-		tmp.calculateQuarters();
-		tmp.calculateDimes();
-		tmp.calculateNickels();
-		tmp.calculatePennies();
-		tmp.calculateTotalChange(price, paid);
+		VendingMachine vm = new VendingMachine();
+		vm.calculateQuarters();
+		vm.calculateDimes();
+		vm.calculateNickels();
+		vm.calculatePennies();
+		vm.calculateTotalChange(price, paid);
 		
-		Change ch = new Change(tmp.getQuarters(), tmp.getDimes(), tmp.getNickels(), tmp.getPennies());
+		Change ch = new Change(vm.quarters, vm.dimes, vm.nickels, vm.pennies);
 		return ch;
 	}
 }
