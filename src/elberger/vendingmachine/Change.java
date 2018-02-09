c745a1a0d872220359ded0437cb68a96e2b83c08
Rachel Elberger <relberger96@gmail.com>
2018-02-08 20:02:18 -0500

    Added .gitignorevendingmachine/Change

--- src/elberger/vendingmachine/Change.java
+++ src/elberger/vendingmachine/Change.java
@@ -6,10 +6,6 @@ public class Change
 	int nickels; 
 	int dimes;
 	int pennies;
-	double remainingChange;
-	double price; 
-	double paid; 
-	double totalChange;
 
 	public Change(int quarters, int dimes, int nickels, int pennies)
 	{
@@ -19,41 +15,7 @@ public class Change
 		this.pennies = pennies;
 	}
 	
-	public double calculateTotalChange(double price, double paid)
-	{
-		totalChange = paid - price;
-		return totalChange;
-	}
-	public double calculateRemainingChange(int coin)
-	{
-		remainingChange = totalChange - coin;
-		return remainingChange;
-	}
 	
-	public int calculateQuarters()
-	{
-		quarters = (int) (totalChange / 25);
-		return quarters;
-	}
-
-	public int calculateDimes()
-	{
-		dimes = (int) (calculateRemainingChange(quarters) / 10);
-		return dimes;
-	}
-
-	public int calculateNickels()
-	{
-		nickels = (int) (calculateRemainingChange(dimes) / 5);
-		return nickels;
-	}
-
-	public int calculatePennies()
-	{
-		pennies = (int) (calculateRemainingChange(nickels) / 1);
-		return pennies;
-	}
-
 	public int getQuarters()
 	{
 		return quarters;
