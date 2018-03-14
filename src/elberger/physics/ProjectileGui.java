9c23640864096822e25d232611d459311fb5888b
Rachel Elberger <relberger96@gmail.com>
2018-03-13 22:34:23 -0400

    projectile gui

--- src/elberger/physics/ProjectileGui.java
+++ src/elberger/physics/ProjectileGui.java
@@ -1,18 +1,19 @@
 package elberger.physics;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 
-public class ProjectileGui extends JFrame implements PropertyChangeListener
+public class ProjectileGui extends JFrame implements DocumentListener
 {
 
-	private JFormattedTextField angle;
-	private JFormattedTextField velocity;
-	private JFormattedTextField time;
-	private JFormattedTextField xCoordinate;
-	private JFormattedTextField yCoordinate;
+	private JTextField angle;
+	private JTextField velocity;
+	private JTextField time;
+	private JTextArea xCoordinate;
+	private JTextArea yCoordinate;
 
 	public ProjectileGui()
 	{
@@ -23,11 +24,11 @@ public class ProjectileGui extends JFrame implements PropertyChangeListener
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-		angle = new JFormattedTextField("");
-		velocity = new JFormattedTextField("");
-		time = new JFormattedTextField("");
-		xCoordinate = new JFormattedTextField("");
-		yCoordinate = new JFormattedTextField("");
+		angle = new JTextField("");
+		velocity = new JTextField("");
+		time = new JTextField("");
+		xCoordinate = new JTextArea("");
+		yCoordinate = new JTextArea("");
 
 		panel.add(new JLabel("Angle:"));
 		panel.add(angle);
@@ -40,33 +41,78 @@ public class ProjectileGui extends JFrame implements PropertyChangeListener
 		panel.add(xCoordinate);
 		panel.add(new JLabel("Y Coordinate:"));
 		panel.add(yCoordinate);
-		
+
 		xCoordinate.setEditable(false);
 		yCoordinate.setEditable(false);
 
-		angle.addPropertyChangeListener("value", this::propertyChange);
-		velocity.addPropertyChangeListener("value", this::propertyChange);
-		time.addPropertyChangeListener("value", this::propertyChange);
-		
+		time.getDocument().addDocumentListener(this);
+		time.getDocument().putProperty("", "");
+
 		add(panel);
 
 	}
 
-	public void propertyChange(PropertyChangeEvent event)
+	@Override
+	public void insertUpdate(DocumentEvent e)
 	{
+		Document doc = (Document) e.getDocument();
+		try
+		{
+			Double.parseDouble((doc.getText(0, doc.getLength())));
+		} 
+		catch (NumberFormatException | BadLocationException e1)
+		{
+			e1.printStackTrace();
+		}
+
 		double angleDouble = Double.parseDouble(angle.getText());
 		double velocityDouble = Double.parseDouble(velocity.getText());
 		double timeDouble = Double.parseDouble(time.getText());
-		
-		Projectile pp = new Projectile (angleDouble, velocityDouble);
-				
-		xCoordinate.setValue(pp.getX(timeDouble));
-		yCoordinate.setValue(pp.getY(timeDouble));			
+
+		Projectile pp = new Projectile(angleDouble, velocityDouble);
+
+		String xCoo = String.valueOf(pp.getX(timeDouble));
+		String yCoo = String.valueOf(pp.getY(timeDouble));
+
+		xCoordinate.setText(xCoo);
+		yCoordinate.setText(yCoo);
+
+	}
+
+	@Override
+	public void changedUpdate(DocumentEvent e)
+	{
+
+	}
+
+	@Override
+	public void removeUpdate(DocumentEvent e)
+	{
+		Document doc = (Document) e.getDocument();
+		try
+		{
+			Double.parseDouble((doc.getText(0, doc.getLength())));
+		} 
+		catch (NumberFormatException | BadLocationException e1)
+		{
+			e1.printStackTrace();
+		}
+
+		double angleDouble = Double.parseDouble(angle.getText());
+		double velocityDouble = Double.parseDouble(velocity.getText());
+		double timeDouble = Double.parseDouble(time.getText());
+
+		Projectile pp = new Projectile(angleDouble, velocityDouble);
+
+		String xCoo = String.valueOf(pp.getX(timeDouble));
+		String yCoo = String.valueOf(pp.getY(timeDouble));
+
+		xCoordinate.setText(xCoo);
+		yCoordinate.setText(yCoo);
 	}
 
 	public static void main(String[] args)
 	{
 		new ProjectileGui().setVisible(true);
 	}
-
 }
