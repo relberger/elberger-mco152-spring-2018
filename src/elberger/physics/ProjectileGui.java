16dbc038125323b64ba3afb7b3097f616887b3f8
Rachel Elberger <relberger96@gmail.com>
2018-03-13 23:14:41 -0400

    projectile gui

--- src/elberger/physics/ProjectileGui.java
+++ src/elberger/physics/ProjectileGui.java
@@ -12,8 +12,8 @@ public class ProjectileGui extends JFrame implements DocumentListener
 	private JTextField angle;
 	private JTextField velocity;
 	private JTextField time;
-	private JTextArea xCoordinate;
-	private JTextArea yCoordinate;
+	private JTextField xCoordinate;
+	private JTextField yCoordinate;
 
 	public ProjectileGui()
 	{
@@ -27,8 +27,9 @@ public class ProjectileGui extends JFrame implements DocumentListener
 		angle = new JTextField("");
 		velocity = new JTextField("");
 		time = new JTextField("");
-		xCoordinate = new JTextArea("");
-		yCoordinate = new JTextArea("");
+		xCoordinate = new JTextField("");
+		yCoordinate = new JTextField("");
+		
 
 		panel.add(new JLabel("Angle:"));
 		panel.add(angle);
@@ -45,6 +46,10 @@ public class ProjectileGui extends JFrame implements DocumentListener
 		xCoordinate.setEditable(false);
 		yCoordinate.setEditable(false);
 
+		angle.getDocument().addDocumentListener(this);
+//		angle.getDocument().putProperty("", "");
+		velocity.getDocument().addDocumentListener(this);
+		velocity.getDocument().putProperty("", "");
 		time.getDocument().addDocumentListener(this);
 		time.getDocument().putProperty("", "");
 
@@ -55,10 +60,28 @@ public class ProjectileGui extends JFrame implements DocumentListener
 	@Override
 	public void insertUpdate(DocumentEvent e)
 	{
-		Document doc = (Document) e.getDocument();
+		Document angleDoc = (Document) e.getDocument();
 		try
 		{
-			Double.parseDouble((doc.getText(0, doc.getLength())));
+			Double.parseDouble((angleDoc.getText(0, angleDoc.getLength())));
+		} 
+		catch (NumberFormatException | BadLocationException e1)
+		{
+			e1.printStackTrace();
+		}
+		Document velocityDoc = (Document) e.getDocument();
+		try
+		{
+			Double.parseDouble((velocityDoc.getText(0, velocityDoc.getLength())));
+		} 
+		catch (NumberFormatException | BadLocationException e1)
+		{
+			e1.printStackTrace();
+		}
+		Document timeDoc = (Document) e.getDocument();
+		try
+		{
+			Double.parseDouble((timeDoc.getText(0, timeDoc.getLength())));
 		} 
 		catch (NumberFormatException | BadLocationException e1)
 		{
@@ -82,16 +105,73 @@ public class ProjectileGui extends JFrame implements DocumentListener
 	@Override
 	public void changedUpdate(DocumentEvent e)
 	{
+		Document angleDoc = (Document) e.getDocument();
+		try
+		{
+			Double.parseDouble((angleDoc.getText(0, angleDoc.getLength())));
+		} 
+		catch (NumberFormatException | BadLocationException e1)
+		{
+			e1.printStackTrace();
+		}
+		Document velocityDoc = (Document) e.getDocument();
+		try
+		{
+			Double.parseDouble((velocityDoc.getText(0, velocityDoc.getLength())));
+		} 
+		catch (NumberFormatException | BadLocationException e1)
+		{
+			e1.printStackTrace();
+		}
+		Document timeDoc = (Document) e.getDocument();
+		try
+		{
+			Double.parseDouble((timeDoc.getText(0, timeDoc.getLength())));
+		} 
+		catch (NumberFormatException | BadLocationException e1)
+		{
+			e1.printStackTrace();
+		}
+
+		double angleDouble = Double.parseDouble(angle.getText());
+		double velocityDouble = Double.parseDouble(velocity.getText());
+		double timeDouble = Double.parseDouble(time.getText());
+
+		Projectile pp = new Projectile(angleDouble, velocityDouble);
+
+		String xCoo = String.valueOf(pp.getX(timeDouble));
+		String yCoo = String.valueOf(pp.getY(timeDouble));
+
+		xCoordinate.setText(xCoo);
+		yCoordinate.setText(yCoo);
 
 	}
 
 	@Override
 	public void removeUpdate(DocumentEvent e)
 	{
-		Document doc = (Document) e.getDocument();
+		Document angleDoc = (Document) e.getDocument();
 		try
 		{
-			Double.parseDouble((doc.getText(0, doc.getLength())));
+			Double.parseDouble((angleDoc.getText(0, angleDoc.getLength())));
+		} 
+		catch (NumberFormatException | BadLocationException e1)
+		{
+			e1.printStackTrace();
+		}
+		Document velocityDoc = (Document) e.getDocument();
+		try
+		{
+			Double.parseDouble((velocityDoc.getText(0, velocityDoc.getLength())));
+		} 
+		catch (NumberFormatException | BadLocationException e1)
+		{
+			e1.printStackTrace();
+		}
+		Document timeDoc = (Document) e.getDocument();
+		try
+		{
+			Double.parseDouble((timeDoc.getText(0, timeDoc.getLength())));
 		} 
 		catch (NumberFormatException | BadLocationException e1)
 		{
