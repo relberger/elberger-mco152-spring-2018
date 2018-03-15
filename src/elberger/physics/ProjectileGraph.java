8fe618ec45c048228dabc85232e27c15f6909e70
Rachel Elberger <relberger96@gmail.com>
2018-03-14 20:15:46 -0400

    projectile lines question

--- src/elberger/physics/ProjectileGraph.java
+++ src/elberger/physics/ProjectileGraph.java
@@ -3,6 +3,8 @@ package elberger.physics;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import java.util.ArrayList;
+
 import javax.swing.JComponent;
 
 public class ProjectileGraph extends JComponent
@@ -13,27 +15,50 @@ public class ProjectileGraph extends JComponent
 	{
 		super.paintComponent(g);
 
-		g.drawLine(10, 80, 400, 260);
+		//g.drawLine(10, 80, 400, 260);
 
-		g.drawOval(50, 100, 200, 80);
+		//g.drawOval(50, 100, 200, 80);
 
-		g.setColor(Color.RED);
+		///g.setColor(Color.RED);
 
-		g.fillRect(400, 100, 50, 150);
+		//g.fillRect(400, 100, 50, 150);
 
-		g.drawString("Here's a String", 125, 300);
+		//g.drawString("Here's a String", 125, 300);
 
-		Projectile pp = new Projectile(50, 100);
+		Projectile projectile1 = new Projectile(50, 85);
 		
 		g.translate(0, getHeight());	//height of graph
-		
-		for (int ii = 0; ii < 15; ii++)
-		{
-			int xx = (int) pp.getX(ii);
-			int yy = -(int) pp.getY(ii);
-			g.drawOval(xx, yy, 10, 10);
+		drawProjectilePath(g, projectile1);
+
+
 		}
 		
+		public void drawProjectilePath(Graphics gg, Projectile pp)
+		{
+			ArrayList <Integer> xVals = new ArrayList<Integer>();
+			ArrayList <Integer> yVals = new ArrayList<Integer>();
+
+			for (int ii = 0; ii < 15; ii++)
+			{
+				int xx = (int) pp.getX(ii);
+				int yy = -(int) pp.getY(ii);
+				
+				gg.setColor(Color.MAGENTA);
+				gg.fillOval(xx, yy, 10, 10);
+				
+				xVals.add(xx);
+				yVals.add(yy);
+				
+				gg.setColor(Color.BLUE);
+				gg.drawString(xx + ", " + yy, xx - 15, yy - 15);
+							
+			}
+			
+			for (int xy = 0; xy < xVals.size(); xy++)
+			{
+				gg.setColor(Color.YELLOW);
+				gg.drawLine(xVals.get(xy), -yVals.get(xy), xVals.get(xy + 1), -yVals.get(xy + 1));				
+			}
 		
 	}
 
