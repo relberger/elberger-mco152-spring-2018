b92c8f4e939b655dd8efbc57cc3ad2fa96dc699b
Rachel Elberger <relberger96@gmail.com>
2018-04-25 22:47:39 -0400

    updated gui

--- src/elberger/earthquake/net/EarthquakeGUI.java
+++ src/elberger/earthquake/net/EarthquakeGUI.java
@@ -18,6 +18,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class EarthquakeGUI extends JFrame
 {
+	private static final long serialVersionUID = 6111006689421939040L;
 	private JButton button = new JButton("Find largest earthquakes in past month, week, day, and hour");
 	private JTextField monthMag = new JTextField();
 	private JTextField monthLoc = new JTextField();
@@ -43,7 +44,7 @@ public class EarthquakeGUI extends JFrame
 	public EarthquakeGUI()
 	{
 		setTitle("Largest Earthquakes");
-		setSize(1500, 200);
+		setSize(700, 400);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		JPanel panel = new JPanel();
@@ -52,37 +53,29 @@ public class EarthquakeGUI extends JFrame
 		panel.add(button, BorderLayout.NORTH);
 		
 		JPanel earthquakePanel = new JPanel();
-		earthquakePanel.setLayout(new GridLayout(4, 5));
+		earthquakePanel.setLayout(new GridLayout(8, 2));
 		
-		JLabel monthLabel = new JLabel("Past month:");
-		JLabel monthMagLabel = new JLabel("Magnitude:");
-		JLabel monthLocLabel = new JLabel("    Location:");
-		JLabel weekLabel = new JLabel("Past week:");
-		JLabel weekMagLabel = new JLabel("Magnitude:");
-		JLabel weekLocLabel = new JLabel("    Location:");
-		JLabel dayLabel = new JLabel("Past day:");
-		JLabel dayMagLabel = new JLabel("Magnitude:");
-		JLabel dayLocLabel = new JLabel("    Location:");
-		JLabel hourLabel = new JLabel("Past hour:");
-		JLabel hourMagLabel = new JLabel("Magnitude:");
-		JLabel hourLocLabel = new JLabel("    Location:");
+		JLabel monthMagLabel = new JLabel("Past month magnitude:");
+		JLabel monthLocLabel = new JLabel("Past month location:");
+		JLabel weekMagLabel = new JLabel("Past week magnitude:");
+		JLabel weekLocLabel = new JLabel("Past week location:");
+		JLabel dayMagLabel = new JLabel("Past day magnitude:");
+		JLabel dayLocLabel = new JLabel("Past day location:");
+		JLabel hourMagLabel = new JLabel("Past hour magnitude:");
+		JLabel hourLocLabel = new JLabel("Past hour location:");
 
-		earthquakePanel.add(monthLabel);
 		earthquakePanel.add(monthMagLabel);
 		earthquakePanel.add(monthMag);
 		earthquakePanel.add(monthLocLabel);
 		earthquakePanel.add(monthLoc);
-		earthquakePanel.add(weekLabel);
 		earthquakePanel.add(weekMagLabel);
 		earthquakePanel.add(weekMag);
 		earthquakePanel.add(weekLocLabel);
 		earthquakePanel.add(weekLoc);
-		earthquakePanel.add(dayLabel);
 		earthquakePanel.add(dayMagLabel);
 		earthquakePanel.add(dayMag);
 		earthquakePanel.add(dayLocLabel);
 		earthquakePanel.add(dayLoc);		
-		earthquakePanel.add(hourLabel);
 		earthquakePanel.add(hourMagLabel);
 		earthquakePanel.add(hourMag);
 		earthquakePanel.add(hourLocLabel);
