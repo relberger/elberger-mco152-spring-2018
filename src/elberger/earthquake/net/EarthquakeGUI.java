765c721cdb16bdacaac806591f40a02f32bec569
Rachel Elberger <relberger96@gmail.com>
2018-04-25 22:39:20 -0400

    functional earthquake gui

--- src/elberger/earthquake/net/EarthquakeGUI.java
+++ src/elberger/earthquake/net/EarthquakeGUI.java
@@ -2,8 +2,19 @@ package elberger.earthquake.net;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.util.Comparator;
+import java.util.Optional;
 
 import javax.swing.*;
+
+import elberger.earthquake.Earthquake;
+import elberger.earthquake.EarthquakeFeed;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class EarthquakeGUI extends JFrame
 {
@@ -17,10 +28,22 @@ public class EarthquakeGUI extends JFrame
 	private JTextField hourMag = new JTextField();
 	private JTextField hourLoc = new JTextField();
 	
+	Retrofit retrofit = new Retrofit.Builder()
+			.baseUrl("https://earthquake.usgs.gov")
+			.addConverterFactory(GsonConverterFactory.create())
+			.build();
+	
+	UsgsEarthquakeService service = retrofit.create(UsgsEarthquakeService.class);
+	
+	Call<EarthquakeFeed> callMonth = service.getAllMonth();
+	Call<EarthquakeFeed> callWeek = service.getAllWeek();
+	Call<EarthquakeFeed> callDay = service.getAllDay();
+	Call<EarthquakeFeed> callHour = service.getAllHour();
+	
 	public EarthquakeGUI()
 	{
 		setTitle("Largest Earthquakes");
-		setSize(400, 200);
+		setSize(1500, 200);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		JPanel panel = new JPanel();
@@ -49,19 +72,16 @@ public class EarthquakeGUI extends JFrame
 		earthquakePanel.add(monthMag);
 		earthquakePanel.add(monthLocLabel);
 		earthquakePanel.add(monthLoc);
-
 		earthquakePanel.add(weekLabel);
 		earthquakePanel.add(weekMagLabel);
 		earthquakePanel.add(weekMag);
 		earthquakePanel.add(weekLocLabel);
 		earthquakePanel.add(weekLoc);
-
 		earthquakePanel.add(dayLabel);
 		earthquakePanel.add(dayMagLabel);
 		earthquakePanel.add(dayMag);
 		earthquakePanel.add(dayLocLabel);
 		earthquakePanel.add(dayLoc);		
-
 		earthquakePanel.add(hourLabel);
 		earthquakePanel.add(hourMagLabel);
 		earthquakePanel.add(hourMag);
@@ -77,12 +97,128 @@ public class EarthquakeGUI extends JFrame
 		hourMag.setEditable(false);
 		hourLoc.setEditable(false);
 		
+		button.addActionListener(e -> 
+		{
+			getMonthValues(e);
+			getWeekValues(e);
+			getDayValues(e);
+			getHourValues(e);
+		});
+		
 		panel.add(earthquakePanel, BorderLayout.CENTER);
 		
 		add(panel);
 
 	}
 
+	public void getMonthValues(ActionEvent e)
+	{
+		callMonth.enqueue(new Callback<EarthquakeFeed>()
+				{
+					@Override
+					public void onResponse(Call<EarthquakeFeed> callMonth, Response<EarthquakeFeed> responseMonth)
+					{
+						EarthquakeFeed feedMonth = responseMonth.body();
+						
+						Optional<Earthquake> greatestMonth = feedMonth.getFeatures()
+								.stream()
+								.max(Comparator.comparing(e -> e.getProperties().getMag()));
+							
+							String monthMagValue = String.valueOf(greatestMonth.get().getProperties().getMag());
+							String monthLocValue = String.valueOf(greatestMonth.get().getProperties().getPlace());
+							monthMag.setText(monthMagValue);
+							monthLoc.setText(monthLocValue);
+					}
+					
+					@Override
+					public void onFailure(Call<EarthquakeFeed> callMonth, Throwable t)
+					{
+						t.printStackTrace();
+					}
+				});
+	}
+	
+	public void getWeekValues(ActionEvent e)
+	{
+		callWeek.enqueue(new Callback<EarthquakeFeed>()
+				{
+					@Override
+					public void onResponse(Call<EarthquakeFeed> callWeek, Response<EarthquakeFeed> responseWeek)
+					{
+						EarthquakeFeed feedWeek = responseWeek.body();
+						
+						Optional<Earthquake> greatestWeek = feedWeek.getFeatures()
+								.stream()
+								.max(Comparator.comparing(e -> e.getProperties().getMag()));
+							
+							String weekMagValue = String.valueOf(greatestWeek.get().getProperties().getMag());
+							String weekLocValue = String.valueOf(greatestWeek.get().getProperties().getPlace());
+							weekMag.setText(weekMagValue);
+							weekLoc.setText(weekLocValue);
+					}
+					
+					@Override
+					public void onFailure(Call<EarthquakeFeed> callMonth, Throwable t)
+					{
+						t.printStackTrace();
+					}
+				});
+	}
+	
+	public void getDayValues(ActionEvent e)
+	{
+		callDay.enqueue(new Callback<EarthquakeFeed>()
+		{
+			@Override
+			public void onResponse(Call<EarthquakeFeed> callDay, Response<EarthquakeFeed> responseDay)
+			{
+				EarthquakeFeed feedDay = responseDay.body();
+				
+				Optional<Earthquake> greatestDay = feedDay.getFeatures()
+						.stream()
+						.max(Comparator.comparing(e -> e.getProperties().getMag()));
+					
+					String dayMagValue = String.valueOf(greatestDay.get().getProperties().getMag());
+					String dayLocValue = String.valueOf(greatestDay.get().getProperties().getPlace());
+					dayMag.setText(dayMagValue);
+					dayLoc.setText(dayLocValue);
+			}
+			
+			@Override
+			public void onFailure(Call<EarthquakeFeed> callDay, Throwable t)
+			{
+				t.printStackTrace();
+			}
+		});
+	}
+	
+	public void getHourValues(ActionEvent e)
+	{
+		callHour.enqueue(new Callback<EarthquakeFeed>()
+		{
+			@Override
+			public void onResponse(Call<EarthquakeFeed> callHour, Response<EarthquakeFeed> responseHour)
+			{
+				EarthquakeFeed feedHour = responseHour.body();
+				
+				Optional<Earthquake> greatestHour = feedHour.getFeatures()
+					.stream()
+					.max(Comparator.comparing(e -> e.getProperties().getMag()));
+				
+				String hourMagValue = String.valueOf(greatestHour.get().getProperties().getMag());
+				String hourLocValue = String.valueOf(greatestHour.get().getProperties().getPlace());
+				hourMag.setText(hourMagValue);
+				hourLoc.setText(hourLocValue);
+			}
+			
+			@Override
+			public void onFailure(Call<EarthquakeFeed> callHour, Throwable t)
+			{
+				t.printStackTrace();
+			}
+		});
+		
+	}
 	public static void main(String[] args)
 	{
 		new EarthquakeGUI().setVisible(true);
